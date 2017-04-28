f8f8bba59cdb346ee6bd5df1932d316439366367
Takashi Masuda <masutaka.net@gmail.com>
2017-04-28 15:23:20 +0900

    Revert "Improve docker command"

--- run-docker.sh
+++ run-docker.sh
@@ -5,6 +5,6 @@ if [ $(whoami) != dev ]; then
   exit 1
 fi
 
-docker run --rm -p 8080:8080 -p 50000:50000 \
+docker run -p 8080:8080 -p 50000:50000 \
        -v /home/dev/socialplus-jenkins/var:/var/jenkins_home \
        -d --restart always feedforce/socialplus-jenkins
