b73a7daafff39f84487e9e9c5305a54b0881f49b
Tsubasa Takayama <t-takayama@feedforce.jp>
2018-11-09 15:48:35 +0900

    Use docker-compose in run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -1,10 +1,4 @@
 #!/bin/sh -eu
 
-if [ $(whoami) != dev ]; then
-  echo "it should be `dev` user." >&2
-  exit 1
-fi
-
-docker run -p 8080:8080 -p 50000:50000 \
-       -v /home/dev/socialplus-jenkins/var:/var/jenkins_home \
-       -d --restart always feedforce/socialplus-jenkins
+docker-compose pull
+docker-compose up -d
